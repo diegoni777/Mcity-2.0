a9aecf735eba115b0b76b8429b6cb4b5e515a12d
DaemynkGJL <65817196+DAEMYNKGJLI@users.noreply.github.com>
2022-06-17 20:48:40 -0500

    registro

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,212 +1,234 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Registrate</title>
-    
-    
-    
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+<html lang="es-mx">
+    <head>
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+        <link rel="stylesheet" href="https://bootswatch.com/5/cyborg/bootstrap.min.css">
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;600&display=swap" rel="stylesheet">
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Registro de Usuarios</title>
+        <script src="{{ asset('js/jquery-3.6.0.min.js') }}"></script>
+        
+    </head>
+    <style>
+    @import "compass/css3";
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+$body-bg: #c1bdba;
+$form-bg: #13232f;
+$white: #ffffff;
 
-    <!--===============================================================================================-->	
-	<link rel="icon" type="image/png" href="{{ asset('img/mcity.png') }}">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="{{ asset('fonts/font-awesome-4.7.0/css/font-awesome.min.css') }}">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="{{ asset('fonts/Linearicons-Free-v1.0.0/icon-font.min.css') }}">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
-<!--===============================================================================================-->	
-	<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
-<!--===============================================================================================-->	
-	<link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="{{ asset('css/util.css') }}">
-	<link rel="stylesheet" type="text/css" href="{{ asset('css/main.css') }}">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <script src="https://kit.fontawesome.com/c26cd2166c.js"></script>
-</head>
+$main: #1ab188;
+$main-light: lighten($main,5%);
+$main-dark: darken($main,5%);
+
+$gray-light: #a0b3b0;
+$gray: #ddd;
+
+$thin: 300;
+$normal: 400;
+$bold: 600;
+$br: 4px;
+
+div {
+  background-color: blue;
+  color: white;
+}
+
+html {
+	overflow-y: scroll; 
+}
+
+body {
+  background:$body-bg;
+  font-family: 'Titillium Web', sans-serif;
+  color: white;
+}
+
+a {
+  text-decoration:none;
+  color:$main;
+  transition:.5s ease;
+  &:hover {
+    color:$main-dark;
+  }
+}
+
+.form {
+  
+  padding: 40px;
+  max-width:600px;
+  margin:40px auto;
+  border-radius:$br;
+}
+
+.tab-group {
+  list-style:none;
+  padding:0;
+  margin:0 0 40px 0;
+  &:after {
+    content: "";
+    display: table;
+    clear: both;
+  }
+  li a {
+    display:block;
+    text-decoration:none;
+    padding:15px;
+    background:rgba($gray-light,.25);
+    color:$gray-light;
+    font-size:20px;
+    float:left;
+    width:50%;
+    text-align:center;
+    cursor:pointer;
+    transition:.5s ease;
+    &:hover {
+      background:$main-dark;
+      color:$white;
+    }
+  }
+  .active a {
+    background:$main;
+    color:$white;
+  }
+}
+
+.tab-content > div:last-child {
+  display:none;
+}
 
 
+h1 {
+  text-align:center;
+  color:$white;
+  font-weight:$thin;
+  margin:0 0 40px;
+}
+
+label.highlight {
+	color:$white;
+}
+
+input, textarea {
+  font-size:22px;
+  display:block;
+  width:100%;
+  height:100%;
+  padding:5px 10px;
+  background:none;
+  background-image:none;
+  border:1px solid $gray-light;
+  color: white;
+  border-radius:0;
+  transition:border-color .25s ease, box-shadow .25s ease;
+  &:focus {
+		outline:0;
+		border-color:$main;
+  }
+}
+
+textarea {
+  border:2px solid $gray-light;
+  resize: vertical;
+}
+
+.top-row {
+  &:after {
+    content: "";
+    display: table;
+    clear: both;
+  }
+}
+
+.button {
+  border:0;
+  outline:none;
+  border-radius:0;
+  padding:15px 0;
+  font-size:2rem;
+  font-weight:$bold;
+  text-transform:uppercase;
+  letter-spacing:.1em;
+  background:$main;
+  color:$white;
+  transition:all.5s ease;
+  -webkit-appearance: none;
+  &:hover, &:focus {
+    background:$main-dark;
+  }
+}
+
+.button-block {
+  display:block;
+  width:100%;
+}
+
+.forgot {
+  margin-top:-20px;
+  text-align:right;
+}
+    </style>
 
 <body>
-
-
-<div class="limiter">
-		<div class="container-login100">
-			<div class="wrap-login100">
-				<form class="login100-form validate-form" action="{{ route('guarda_usuarios') }}" method="POST" enctype="multipart/form-data">
-        {{ csrf_field() }}
-				
-					<span class="login100-form-title p-b-43">
-						Registrate
-					</span>
-					
-          <label for="">Inserta tu Usuario</label><br>
-					<div class="wrap-input100 validate-input" >
-						<input class="input100" type="text" name="nombre" placeholder="Nombre" required>
-					
-					</div>
-
-          <label for="">Empresa</label> <br>
-          <div class="wrap-input100">
-              <input class="input100" type="text" name="empresa" value="" placeholder="Empresa" required>
-          </div><br>
     
-          <label for="">Detalles</label> <br>
-          <div class="wrap-input100">
-              <input class="input100" type="text" name="detalles" value="" placeholder="Detalles" required>
-          </div><br>
-    
-         
-          <div class="wrap-input100" style="display: none;">
-              <input class="input100" type="text" name="tipou" value="usuario" placeholder="Tipo usuario" readonly  required>
-          </div>
-    
-          <label for="">Foto de perfil</label>
-          <div >
-              <input class="input100" type="file" name="fotou" value="" placeholder="ID" >
-          </div><br>
-    
-          <label for="">Correo</label>
-          <div class="wrap-input100">
-              <input class="input100" type="text" name="correo" value="" placeholder="Correo" required>
-          </div><br>
-    
-          <label for="">Password</label>
-          <div class="wrap-input100 validate-input">
-              <input id="password" class="input100"  type="password" name="password" value="" placeholder="Password"  onkeyup="evaluar(this);" required>
-          </div>
-    
+    <h1>Registro Usuarios</h1>
+    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/User_icon_2.svg/1200px-User_icon_2.svg.png" alt="Usuario" width="90px" height="90px" >
+        <div class="container">
+            
+            <form action="{{ route ( 'guarda_usuarios' )}}" method="POST" enctype = 'multipart/form-data'>
+            {{ csrf_field() }}
 
-
-          <b id="nivel"></b><br>
-          <b id="error"></b><br>
-          <br>
-
-					<div class="container-login100-form-btn">
-						<button class="login100-form-btn" >
-							Registrate
-						</button>
-					</div>
-                    <div><a class="span" href="{{ route('/') }}">Salir</a></div>
-					
-					
-
-					
-				</form>
-
-				<div class="login100-more" style="background-image: url('img/slider-3.jpg');">
-				</div>
-			</div>
-		</div>
-	</div>
-
-    <script type="text/javascript">
-                function evaluar(obj){
-                    var pass = obj.value;               //Recibe variable de input
-                    var conteo = obj.value.length;      //Cuenta la cadena string|Conraseña
-                    var cadena = pass.split("");        //Divide la cadena String|Contraseña
-                    var cont = 0;                       //Cuenta los caracteres especiales
-                    var let_a = 0;                      //Cuenta las letras minusculas
-                    var let_b = 0;                      //Cuenta las letras mayusculas
-                    var let_c = 0;                      //Cuenta n     los numeros
-                    var let_d = 0;                        //Caracter
-                    if(conteo > 3){
-                            if(cadena[0].search(/[A-Z]/g)+1 > 0){
-                                let_a = 0;
-                                let_b = 0;
-                                let_c = 0;
-                                let_d = 0;
-                                cont = 0;
-
-                                 //-------------------Evaluar: caracter x caracter
-                                 for(var i=0; i < cadena.length; i++){
-                                     var letra = cadena[i];
-                                     if(letra.search(/[a-z]/g)+1 > 0){ let_a = let_a+1; }
-                                     else{
-                                            if(letra.search(/[A-Z]/g)+1 > 0){ let_b = let_b+1;}
-                                            else{
-                                                    if(letra.search(/[0-9]/g)+1 > 0){let_c = let_c+1;}
-                                                    else{
-                                                        if(letra.search(/[%&#$]/g)+1 > 0){let_d = let_d+1; }
-                                                        else{ cont = cont+1; }
-                                                    }
-                                            }
-                                     }
-                                 }
-                                 //------------------Evaluar: Canitdad de caracteres
-
-                                 if(conteo >=8){
-                                    document.getElementById("nivel").style.cssText = 'color: #000; background-color: #0D8E1A;';
-                                    document.getElementById("nivel").innerHTML = "Segura";
-                                 }else{
-                                        if(conteo >=6 ){
-                                            document.getElementById("nivel").style.cssText = 'color: #000; background-color: #EFDD0F;';
-                                            document.getElementById("nivel").innerHTML = "Pocosegura";
-                                        }
-                                        else{
-                                            document.getElementById("nivel").style.cssText = 'color: #000; background-color: #F00; ';
-                                            document.getElementById("nivel").innerHTML = "Insegura";
-                                        }
-                                 }
-                                 
-                                 //-------------------Indicar Errores: Si existen letras y numeros
-                                 if(let_a < 1 || let_b < 1 || let_c < 1 || let_d < 1){
-                                     if(let_a < 1){let_a = "una letra minuscula"; }else{ let_a = " ";}
-                                     if(let_b < 1){let_b = "una letra mayuscula"; }else{ let_b = " ";}
-                                     if(let_c < 1){let_c = "un numero"; }else{ let_c = " ";}
-                                     if(let_d < 1){let_d = "un caracter = (%&#$)"; }else{ let_d = " ";}
-                                     document.getElementById("error").innerHTML = "Error: la contraseña debe de contener al menos " + let_a + " " + let_b + " " + let_c  + " " + let_d; 
-                                 }else{
-                                     document.getElementById("error").innerHTML = " ";
-                                 }
-                                 // ---------------------- Indica Errores: Si existen caracteres especiales
-                                 if(cont > 0){
-                                    
-                                    document.getElementById("password").style.cssText = 'color: #F00; border: solid 1 px #F00';
-                                    document.getElementById("nivel").innerHTML = " ";
-                                 }
-                                 else{
-                                     document.getElementById("password").style.cssText = 'color: #000; border: solid 1px #000';
-                                 }
-                            }
-                            else{ 
-                                document.getElementById("password").style.cssText = 'color: #F00; border: solid 1px #F00';
-                                document.getElementById("nivel").style.cssText = 'border-style: none;';
-                                document.getElementById("nivel").innerHTML = " ";
-                                document.getElementById("error").innerHTML = "Error: El primer caracter debe ser una letra mayuscula";
-                            }
-                    }
-                    else{
-                        document.getElementById("password").style.cssText = 'color: #000; border: solid 1px #000';
-                        document.getElementById("nivel1").innerHTML = " ";
-                        document.getElementById("error").innerHTML = " ";
-                    }
-                }
-
-               
+                <div class="wrap-input100" style="display: none;">
+                    <label for="">id usuario</label>
+                    <input class="input100" type="text" name="idu" value="{{$idesigue}}" placeholder="ID" disabled>
+                    <span class="focus-input100"></span>
+                        <span class="symbol-input100">
+                            <i class="fa fa-user" aria-hidden="true"></i>
+                        </span>
+                </div><br>
                 
-            </script>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-
-
+                <div class="wrap-input100" style=" color: white">
+                    <label for="">Inserta tu Usuario</label><i class="fa fa-user" aria-hidden="true"></i>
+                    <input class="input100" type="text" name="nombre" value="" placeholder=" Usuario" >
+                </div><br>
+    
+                <div class="wrap-input100">
+                    <label for="">Empresa</label><i class="fa fa-industry" aria-hidden="true"></i>
+                    <input class="input100" type="text" name="empresa" value="" placeholder="Empresa" >
+                </div><br>
+    
+                <div class="wrap-input100">
+                    <label for="">Detalles</label><i class="fa fa-info" aria-hidden="true"></i>
+                    <input class="input100" type="text" name="detalles" value="" placeholder="Detalles" >
+                </div><br>
+    
+                <div class="wrap-input100" style="display: none;">
+                    <label for="">Tipo usuario</label><i class="fa fa-user" aria-hidden="true"></i>
+                    <input class="input100" type="text" name="tipou" value="usuario" placeholder="Tipo usuario" readonly >
+                    
+                </div><br>
+    
+                <div class="wrap-input100">
+                    <label for="">Foto de perfil</label><i class="fa fa-camera" aria-hidden="true"></i>
+                    <input class="input100" type="file" name="fotou" value="" placeholder="ID" >
+                </div><br>
+    
+                <div class="wrap-input100">
+                    <label for="">Correo</label><i class="fa fa-envelope" aria-hidden="true"></i>
+                    <input class="input100" type="text" name="correo" value="" placeholder="Correo" >
+                </div><br>
+    
+                <div class="wrap-input100">
+                    <label for="">Password</label><i class="fa fa-key" aria-hidden="true"></i>
+                    <input class="input100" type="password" name="password" value="" placeholder="Password">
+                </div><br>
+    
+                <div>
+                    <button>guardar usuario</button>
+                </div>
+            </form>
+        </div>
 </body>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-
-
 </html>
