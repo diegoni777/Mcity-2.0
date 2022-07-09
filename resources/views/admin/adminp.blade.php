af17497f554fa50d377657115d55716a1ada111c
DaemynkGJL <65817196+DAEMYNKGJLI@users.noreply.github.com>
2022-07-09 07:36:45 -0500

    cambios adminp y admin

--- resources/views/admin/adminp.blade.php
+++ resources/views/admin/adminp.blade.php
@@ -40,9 +40,9 @@
 								<div class="dropdown-messages-box"><a href="profile.html" class="pull-left">
 									<img alt="image" class="img-circle" src="http://placehold.it/40/30a5ff/fff">
 									</a>
-									<div class="message-body"><small class="pull-right">3 mins ago</small>
-										<a href="#"><strong>John Doe</strong> commented on <strong>your photo</strong>.</a>
-									<br /><small class="text-muted">1:24 pm - 25/03/2015</small></div>
+									<div class="message-body"><small class="pull-right">Hace 3 minutos</small>
+										<a href="#"><strong>John Doe</strong> Comentado <strong>Tu foto</strong>.</a>
+									<br /><small class="text-muted">1:24 pm - 25/03/2022</small></div>
 								</div>
 							</li>
 							<li class="divider"></li>
@@ -50,15 +50,15 @@
 								<div class="dropdown-messages-box"><a href="profile.html" class="pull-left">
 									<img alt="image" class="img-circle" src="http://placehold.it/40/30a5ff/fff">
 									</a>
-									<div class="message-body"><small class="pull-right">1 hour ago</small>
-										<a href="#">New message from <strong>Jane Doe</strong>.</a>
-									<br /><small class="text-muted">12:27 pm - 25/03/2015</small></div>
+									<div class="message-body"><small class="pull-right">Hace 1 hora</small>
+										<a href="#">Nuevo mensaje de  <strong>Jane Doe</strong>.</a>
+									<br /><small class="text-muted">12:27 pm - 25/03/2022</small></div>
 								</div>
 							</li>
 							<li class="divider"></li>
 							<li>
 								<div class="all-button"><a href="#">
-									<em class="fa fa-inbox"></em> <strong>All Messages</strong>
+									<em class="fa fa-inbox"></em> <strong>Todos los mensajes</strong>
 								</a></div>
 							</li>
 						</ul>
@@ -68,13 +68,13 @@
 					</a>
 						<ul class="dropdown-menu dropdown-alerts">
 							<li><a href="#">
-								<div><em class="fa fa-envelope"></em> 1 New Message
-									<span class="pull-right text-muted small">3 mins ago</span></div>
+								<div><em class="fa fa-envelope"></em> 1 mensaje nuevo
+									<span class="pull-right text-muted small">Hace 3 minutos</span></div>
 							</a></li>
 							<li class="divider"></li>
 							<li><a href="#">
-								<div><em class="fa fa-heart"></em> 12 New Likes
-									<span class="pull-right text-muted small">4 mins ago</span></div>
+								<div><em class="fa fa-heart"></em> 12 nuevos me gusta
+									<span class="pull-right text-muted small">Hace 4 minutos</span></div>
 							</a></li>
 							<li class="divider"></li>
 							<li><a href="#">
@@ -90,7 +90,7 @@
 	<div id="sidebar-collapse" class="col-sm-3 col-lg-2 sidebar">
 		<div class="profile-sidebar">
 			<div class="profile-userpic">
-				<img src="http://placehold.it/50/30a5ff/fff" class="img-responsive" alt="">
+			<img src="img/usu9.png" class="img-responsive" alt="">
 			</div>
 			<div class="profile-usertitle">
 				<div class="profile-usertitle-name"><h5>  <?php echo $sessionusuario?> </h5></div>
@@ -159,7 +159,7 @@
 					<div class="panel panel-teal panel-widget border-right">
 						<div class="row no-padding"><em class="fa fa-xl fa-shopping-cart color-blue"></em>
 							<div class="large">120</div>
-							<div class="text-muted">New Orders</div>
+							<div class="text-muted">Nuevas ordenas</div>
 						</div>
 					</div>
 				</div>
@@ -167,7 +167,7 @@
 					<div class="panel panel-blue panel-widget border-right">
 						<div class="row no-padding"><em class="fa fa-xl fa-comments color-orange"></em>
 							<div class="large">52</div>
-							<div class="text-muted">Comments</div>
+							<div class="text-muted">Comentarios</div>
 						</div>
 					</div>
 				</div>
@@ -175,7 +175,7 @@
 					<div class="panel panel-orange panel-widget border-right">
 						<div class="row no-padding"><em class="fa fa-xl fa-users color-teal"></em>
 							<div class="large">24</div>
-							<div class="text-muted">New Users</div>
+							<div class="text-muted">Nuevos usuarios</div>
 						</div>
 					</div>
 				</div>
@@ -183,7 +183,7 @@
 					<div class="panel panel-red panel-widget ">
 						<div class="row no-padding"><em class="fa fa-xl fa-search color-red"></em>
 							<div class="large">25.2k</div>
-							<div class="text-muted">Page Views</div>
+							<div class="text-muted">vistas a la pagina</div>
 						</div>
 					</div>
 				</div>
